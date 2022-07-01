83ccb281dedf3256a9b68154ee839bdf66cd4ae4
Almarat <arnualmarat@gmail.com>
2022-07-01 14:18:32 -0700

    a^b

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def returns_power(a, b):
     return a**b
-print(returns_power(2, 3))
+print(returns_power(2, 2))
+print(returns_power(98, 2))
+print(returns_power(98, 0))
+print(returns_power(100, -2))
+print(returns_power(-4, 5))
