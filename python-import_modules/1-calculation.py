0cc7432eeba04aefd100d7d977c5766b213cfc72
Almarat <arnualmarat@gmail.com>
2022-07-05 12:53:46 -0700

    no 2

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,7 +2,6 @@
 import calculator_1 as cal
 
 
-
 def main():
     a = 10
     b = 5
@@ -10,7 +9,6 @@ def main():
     print("{} - {} = {}".format(a, b, cal.sub(a, b)))
     print("{} * {} = {}".format(a, b, cal.mul(a, b)))
     print("{} / {} = {}".format(a, b, cal.div(a, b)))
-
 
 
 if __name__ == "__main__":
