b5fc4376befec7030fd16d6e3e4512dde6f25549
Almarat <arnualmarat@gmail.com>
2022-07-05 12:24:57 -0700

    number 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,9 +2,9 @@
 from add_0 import add
 
 def main():
- a = 1
- b = 2
- print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
 
 if __name__ == "__main__":
