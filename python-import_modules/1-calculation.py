da2371c2bfe2f92d90e764a24adea39b843cab62
Almarat <arnualmarat@gmail.com>
2022-07-05 12:42:20 -0700

    My first toolbox!

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import calculator as cal
+
+
+
+def main():
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, cal.add(a, b)))
+    print("{} - {} = {}".format(a, b, cal.sub(a, b)))
+    print("{} * {} = {}".format(a, b, cal.mul(a, b)))
+    print("{} / {} = {}".format(a, b, cal.div(a, b)))
+
+
+
+if __name__ == "__main__":
+    main()
