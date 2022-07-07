e5c6e24c7965bd478a544527e09906508baf9e5f
Almarat <arnualmarat@gmail.com>
2022-07-07 01:36:29 -0700

    number 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-def add(*args):
-    return sum(args)
+import sys
+
+
+def main():
+    args = sys.argv[1:]
+    sum = 0
+    for arg in args:
+        sum += int(arg)
+    print(sum)
 
 
 if __name__ == "__main__":
