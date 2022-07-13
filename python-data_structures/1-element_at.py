80dbaedde018812392dc69f6a61a3d0803968774
Almarat <arnualmarat@gmail.com>
2022-07-13 04:07:36 -0700

    number 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     for i in my_list:
-    my_list[]
-print('{}'.format(my_list[0]))
+        if index == - index:
+            return None
+        elif index > range(len(my_list)):
+            return None
+        else:
+            return my_list[]
