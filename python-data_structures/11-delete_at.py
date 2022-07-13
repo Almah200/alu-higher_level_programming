6ea92a1bbdedac9745d8917b06e843e61280183a
Almarat <arnualmarat@gmail.com>
2022-07-13 14:06:20 -0700

    Delete that

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    for i in  my_list:
-        return del my_list[]
+    if idx < 0 or idx > 0:
+        return my_list
+    else:
+        del my_list[idx]
 
