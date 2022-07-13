3349b6c669b67792900c69b3e8030d479d80aae1
Almarat <arnualmarat@gmail.com>
2022-07-13 01:53:38 -0700

    Delete at

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     for i in my_list:
-        print(my_list [] ="")
+        if [edx] = -[edx]
+        return my_list
+    if [edx] > range(len(my_list):
+            return my_list
+    else:
+
