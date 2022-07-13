d5b01327874459949f555beb2371cd3b9e7c2cf9
Almarat <arnualmarat@gmail.com>
2022-07-13 13:58:45 -0700

    Can you C me now?

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in my_string:
-        if i == 99 or i == 65
-    return my_string.remove(i)
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
