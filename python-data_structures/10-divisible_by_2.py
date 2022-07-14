0d5c6b3e3e59cca7f629dd7237135cdb86ff2170
Almarat <arnualmarat@gmail.com>
2022-07-14 14:51:37 -0700

    only by 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for my_list()
     for i in my_list:
         if i % 2 == 0:
-            return True
+            print('True')
         else:
-            return False
+            print('False')
