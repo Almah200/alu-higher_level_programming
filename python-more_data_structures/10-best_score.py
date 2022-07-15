2b0f6a7ae696d19a86cf21dc4b6b4b7be1a97749
Almarat <arnualmarat@gmail.com>
2022-07-15 12:45:40 -0700

    number 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-
 def best_score(a_dictionary):
-    return sorted(a_dictionary)[-1]
+    if not a_dictionary:
+        return (None)
+
+    return (max(a_dictionary, key=a_dictionary.get))
