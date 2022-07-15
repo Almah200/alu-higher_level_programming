1704c20b7bf344086967551ba527b1981ba97ea5
Almarat <arnualmarat@gmail.com>
2022-07-15 06:01:09 -0700

    search and replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i, n in enumerate(my_list):
-        if n == search:
-            my_list[i] = replace
+new_list[:] = [ replace if x == search else if x for x in my_list]
