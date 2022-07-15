66e2f32052ecf3b657838b0c79e52b40abd1684a
Almarat <arnualmarat@gmail.com>
2022-07-15 10:42:35 -0700

    sorted

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+    for i in range(len(sorted(a_dictionary)))[-1]
+    else:
         return None
-
-    ret = list(a_dictionary.keys())[0]
-    big = a_dictionary[ret]
-    for k, v in a dictionary.items():
-        if v > big:
-            bid = v
-            ret = k
-    return (ret)
