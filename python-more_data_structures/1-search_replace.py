cec8452f5b2ee532d30a1bef03441d19583764f1
Almarat <arnualmarat@gmail.com>
2022-07-15 06:18:24 -0700

    search and replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -5,5 +5,5 @@ def search_replace(my_list, search, replace):
         if i == search:
             new_list.append(replace)
         else:
-            new_list.append(my_list)
+            new_list.append(my_list[i])
     return new_list
