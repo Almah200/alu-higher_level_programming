19f5607fd9dee9855ab697abd0c5de392368d0e5
Almarat <arnualmarat@gmail.com>
2022-07-15 11:11:42 -0700

    by 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_dictionary = {}
     for i in range(len(a_dictionary)):
         return i * 2
+    else:
+        new_dictionary.append(i * 2)
+        return new_dictionary
+
