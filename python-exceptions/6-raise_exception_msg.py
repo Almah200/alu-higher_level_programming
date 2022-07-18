00cba1f8190cfd7a4f0780499c2d33e739b8ff26
Almarat <arnualmarat@gmail.com>
2022-07-18 15:26:04 -0700

    raise amessage

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
     try:
+        almah = 1
+        print(alma)
         raise_exception_msg("C is fun")
     except NameError as ne:
         print(ne)
