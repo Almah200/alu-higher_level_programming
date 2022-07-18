6cbbef4a3f2872e1c9e81b077504240c31f90c70
Almarat <arnualmarat@gmail.com>
2022-07-18 15:14:34 -0700

    Integers division with debug

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        return int(a) / int(b)
+        return a / b
     except typeError::
         print ('Not an integer')
     finally:
-        print('{}'.format(safe_print_division(int(a) / int(b))
+        print('{} / {} = {}'.format((a, b, safe_print_division(a, b))
         print('Inside result:')
