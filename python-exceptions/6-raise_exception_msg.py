bc6746896fc7d30ab0d5348c0ce28f6a9f158bf6
Almarat <arnualmarat@gmail.com>
2022-07-18 13:15:10 -0700

    Raise a message

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+def raise_exception_msg(message=""):
+    try:
+        geek = "GeeksforGeeks"
+        return geeksforgeeks
+    except NameError:
+        return "NameError occurred. Some variable isn't defined."
