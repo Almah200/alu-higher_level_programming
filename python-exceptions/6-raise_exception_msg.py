a01c0bff812cf2de456352126c3adf154ee0c5ec
Almarat <arnualmarat@gmail.com>
2022-07-19 12:27:13 -0700

    corrections

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
     try:
-        almah = 1
+        almah = boy
         print(alma)
-        raise_exception_msg("C is fun")
-    except NameError as ne:
-        print(ne)
+    except NameError:
+        print('NameError occured, some variable not defined')
