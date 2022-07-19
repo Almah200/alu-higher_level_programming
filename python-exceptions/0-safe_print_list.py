8d56b1f5e30e18862334f4c1b5a73af7e26f4d5c
Almarat <arnualmarat@gmail.com>
2022-07-19 13:47:21 -0700

    corrections

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-try:
-    for x in my_list:
-        print(x, end='')
-except NameError:
-    print("x is not defined")
-
+    ret = 0
+    for i in range(x):
+        try:
+            print('{}'.format(my_list[i]), end='')
+            ret+=1
+        except IndexError:
+            break
+    print('')
+    return (ret)
