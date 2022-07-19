4e037b205d1dfac8de55635c1e3ea88b0cec12f4
Almarat <arnualmarat@gmail.com>
2022-07-19 13:31:04 -0700

    Raise a message

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    try:
-        almah = boy
-        print(alma)
-    except NameError:
-        print('NameError occured, some variable not defined')
-raise_exception_msg()
+    raiseNameError(message)
