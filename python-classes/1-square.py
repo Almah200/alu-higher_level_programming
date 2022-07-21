5466854bba330305f463c40a687e08f434bed163
Almarat <arnualmarat@gmail.com>
2022-07-21 22:09:36 +0300

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Defined a class.'''
+class Square:
+    ''' initialized private paramaters.'''
+    def __init__(self, size):
+        ''' indicated the private paramaters'''
+        self.size
