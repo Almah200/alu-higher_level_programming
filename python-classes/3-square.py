24c580afcde5fc1bf239d1dffa5d56d81bcbf5ba
Almarat <arnualmarat@gmail.com>
2022-07-22 00:37:43 +0300

    corrections

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -4,10 +4,12 @@ class Square:
     '''Initialized the parameters.'''
     def__init__(self, size=0):
         if not isinstance(size, int):
+            '''Raising and managing erros.'''
             raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
         self.size = size
+        '''created area function.'''
     def area(self):
         '''Return the current area of the square.'''
         return (self.__size * self.__size)
