52dbc68f851166d4c2c5d34679ce2cb07aa4c9c7
Almarat <arnualmarat@gmail.com>
2022-07-21 23:59:08 +0300

    corrections

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,7 +8,6 @@ class Square:
         elif size < 0:
             raise ValueError('size must be >= 0')
         self.size = size
-        ''' created a public instance method.'''
     def area(self):
         '''Return the current area of the square.'''
         return (self.__size * self.__size)
