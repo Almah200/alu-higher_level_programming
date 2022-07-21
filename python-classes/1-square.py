85b54a50d8ea94efff1fffda4d597f4fd3cd2745
Almarat <arnualmarat@gmail.com>
2022-07-21 22:17:55 +0300

    corrections

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,6 +2,7 @@
 '''Defined a class.'''
 class Square:
     ''' initialized private paramaters.'''
-    def __init__(self,size):
+    def __init__(self,length,width):
         ''' indicated the private paramaters'''
-        self.size = size
+        self.length = length
+        self.width = width
