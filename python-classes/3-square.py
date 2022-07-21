04ad30216060b8c8770529203e9006159786b1a6
Almarat <arnualmarat@gmail.com>
2022-07-22 02:27:11 +0300

    Area of a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
-"""
 
-define a square by:
-Private instance attribute: size
-Instantiation with optional size: def __init__(self, size=0):
-size must be int, raise TypeError exception(message size must be an integer)
-if size is < 0, raise ValueError exception(message size must be >= 0)
-Public instance method: def area(self): returns the current square area You are not allowed to import any module
-"""
+"""Define a class Square."""
 
 
 class Square:
-    """Defined a square."""
-    def__init__(self, size=0):
-         """
-                Creates an instance of Square
-                Args:
-                    size: size of the square
-                """
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
         if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError('size must be >= 0')
-
-        self.size = size
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
+        """Return the current area of the square."""
         return (self.__size * self.__size)
