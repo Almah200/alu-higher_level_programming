875fc1c8dcbace25f5cb4d60ad2a3815c6c6a3e9
Almarat <arnualmarat@gmail.com>
2022-07-28 01:36:45 +0300

    100

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def repeat(word, n, sep=', '):
-    return sep.join(word for _ in range(n))
+def magic_string():
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool"))
