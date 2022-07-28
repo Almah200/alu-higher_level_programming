7684b6ad9d4ea723e1f254411ba297f556a54ed3
Almarat <arnualmarat@gmail.com>
2022-07-28 21:23:01 +0300

    4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 '''.imported json.'''
 import json
-'''. defined a function .'''
 
 
 def from_json_string(my_str):
-    return json.load(my_str)
+    '''return the python object representatio of json.'''
+    return json.loads(my_str)
