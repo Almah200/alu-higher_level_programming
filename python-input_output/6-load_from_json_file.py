ea378ccc7d7b6058eb8c829c61a8600d7fa9813d
Almarat <arnualmarat@gmail.com>
2022-07-28 23:30:34 +0300

    6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''.imported a json file.'''
+import json
+
+
+def load_from_json_file(filename):
+    '''creating a python object from json file.'''
+    with open(filename) as afile:
+        json.load(afile)
