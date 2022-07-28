cfd28fdc9a45dc1a7554bdaf17fbb0cd83b5fac4
Almarat <arnualmarat@gmail.com>
2022-07-28 12:28:55 +0300

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,11 +5,12 @@
 def write_file(filename="", text=""):
     """Write a string to a UTF8 text file.
 
+
     Args:
         filename (str): The name of the file to write.
         text (str): The text to write to the file.
     Returns:
         The number of characters written.
     """
-    with open(filename, 'w', encoding= 'utf-8') as almah:
+    with open(filename, 'w', encoding=i 'utf-8') as almah:
         return almah.write(text)
