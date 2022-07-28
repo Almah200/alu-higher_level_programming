0dcff7b21901c6ae6ef475f1b1f6fbd242a521f0
Almarat <arnualmarat@gmail.com>
2022-07-28 12:27:09 +0300

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -11,5 +11,5 @@ def write_file(filename="", text=""):
     Returns:
         The number of characters written.
     """
-    with open(filename, 'w', encoding = 'utf-8') as almah:
+    with open(filename, 'w', encoding= 'utf-8') as almah:
         return almah.write(text)
