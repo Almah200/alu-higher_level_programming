d7c7fe79ed33d980755c28974382921dda431bbe
Almarat <arnualmarat@gmail.com>
2022-07-28 12:49:49 +0300

    3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''importing json.'''
+import json
+''''defined json function.'''
+
+
+def to_json_string(my_obj):
+    '''returning  a json representation of the object.'''
+    return json.dumps(my_obj)
