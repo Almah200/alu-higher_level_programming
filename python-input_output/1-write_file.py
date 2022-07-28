c104da33eca8b1f942c9dfef713b8f33a0d10861
Almarat <arnualmarat@gmail.com>
2022-07-28 12:21:11 +0300

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 '''defined a function.'''
+
+
 def write_file(filename="", text=""):
     ''' opened a file for writing.'''
     with open(filename, mode='w', encoding = 'utf-8') as almah:
