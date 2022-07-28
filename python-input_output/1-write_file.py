fa3c2f548eca04a5849ce6a5e8f43315f0a901b0
Almarat <arnualmarat@gmail.com>
2022-07-28 12:15:45 +0300

    1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,4 +3,4 @@
 def write_file(filename="", text=""):
     ''' opened a file for writing.'''
     with open(filename, mode='w') as almah:
-        almah.write('Almah is a good boy')
+        almah.write('Almah is a good boy\n')
