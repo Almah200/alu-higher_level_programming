aba5b1421ddc9d2c86c90a433ab1672066ac27dc
Almarat <arnualmarat@gmail.com>
2022-07-29 00:09:20 +0300

    9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,11 +4,19 @@
 
 class Student:
     '''.instatiated a class.'''
+
     def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-        '''.defined a public method.'''
     def to_json(self):
-        return student.__dict__
+        '''get a dictionary representation of the student.'''
+        return self.__dict__
