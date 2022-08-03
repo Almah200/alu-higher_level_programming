6386c54e6e7195c7c42b09fce4f7d8215ff86884
Almarat <arnualmarat@gmail.com>
2022-08-03 15:11:05 +0300

    4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,6 @@
 -- creating a table first_table in the current database in your MySQL server.
-CREATE TABLE IF NOT EXISTS first_table (
-	id INT,
-	name VARCHAR(256),
+CREATE TABLE IF NOT EXISTS first_table 
+(
+id INT,
+name VARCHAR(256),
 );
