02c25cee32d6862ce54e4570a3438423fd6f7e9f
Almarat <arnualmarat@gmail.com>
2022-08-05 02:40:43 +0300

    1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creating a user and granting privileges
-CREATE USER IF NOT EXIST user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
+GRANT ALL ON *.* TO user_0d_1@localhost;
