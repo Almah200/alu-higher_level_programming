4bb7dd07bedeffd7adf484e2068d2c30e900a2c6
Almarat <arnualmarat@gmail.com>
2022-08-05 04:20:59 +0300

    7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
 -- creates a database and atable with pry and foreign key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT UNIQUE AUTO_GENERATED NOT NULL PRIMARY KEY, state_id INT NOT NULL FOREIGN KEY (id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, FOREIGN KEY (state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
 ~
