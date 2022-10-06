1e3a4bbff46c2bcfab81df560ac062718e63b927
Almarat <arnualmarat@gmail.com>
2022-10-06 19:05:56 +0300

    13

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-function add(){
-	const n1 = 10;
-	const n2 = 20;
-
-	const sum = n1 + n2;
-
-	console.log(n1 + n2);
+function add (a, b) {
+	return a + b;
 }
-add();
+
+module.exports = { add };
