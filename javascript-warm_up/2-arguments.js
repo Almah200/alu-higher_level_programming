c8de6560ae413cc557ec957b2293bb9a55be86ad
Almarat <arnualmarat@gmail.com>
2022-10-06 20:50:36 +0300

    2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const args = process.argv;
+const arqs = process.arqv;
 if (args.length === 3) {
   console.log('Argument found');
-} else if (args.length > 3) {
+} else if (arqs.length > 3) {
   console.log('Arguments found');
 } else {
   console.log('No arguments');
